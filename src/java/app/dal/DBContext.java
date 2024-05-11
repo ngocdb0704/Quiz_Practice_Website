ae33eb326bb618c4af83844cbe72394ff7386333
Nguyen Quoc Huy (K18 HL) <huynqhe186861@fpt.edu.vn>
2024-05-11 10:15:59 +0700

    init branch huynq

---
+++ src/java/app/dal/DBContext.java
@@ -0,0 +1,27 @@
+package app.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBContext {
+
+    public Connection connection;
+
+    public DBContext() {
+        try {
+            //Change the username password and url to connect your own database
+            String username = "sa";
+            String password = "123";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=QuizPractice";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+            connection.setAutoCommit(false);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
