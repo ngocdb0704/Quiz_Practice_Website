bf928ec8a4a070f84213eee3395fbfb665bc3b44
Le Thanh An (K18HL) <anlthe182228@fpt.edu.vn>
2024-05-16 16:33:14 +0700

    uwu

--- src/java/app/controller/LoginControllerTempOfAnForLoginView.java
+++ src/java/app/controller/LoginControllerTempOfAnForLoginView.java
@@ -4,12 +4,16 @@
  */
 package app.controller;
 
+import app.entity.User;
+import app.model.DAOUser;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.Vector;
 
 /**
  *
@@ -17,16 +21,76 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 public class LoginControllerTempOfAnForLoginView extends HttpServlet {
 
-   @Override
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        request.getRequestDispatcher("LoginInterface.jsp").forward(request, response);
-    } 
+            throws ServletException, IOException {
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        request.getRequestDispatcher("LoginInterface.jsp").forward(request, response);
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String service = request.getParameter("service");
+        DAOUser daoUser = new DAOUser();
+        if (service.equals("login")) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            boolean flag = validateUser(username, password);
+            if (flag) {
+                String message = "Hello " + username + ". You logged in successfully";
+                session.setAttribute("successMessage", message);
+                
+                response.sendRedirect("index.jsp");
+            } else {
+                response.sendRedirect("LoginInterface.jsp");
+            }
+        }
+        
+        if (service.equals("changepass")) {
+            String username = request.getParameter("username");
+            String prePassword = request.getParameter("prePass");
+            String newPassword = request.getParameter("newPass");
+            String confirmPassword = request.getParameter("confirmPass");
+            boolean flag = validateUser(username, prePassword);
+            if (flag) {
+                if (!(newPassword.equals(prePassword) || newPassword.isEmpty())) {
+                    if (confirmPassword.equals(newPassword)) {
+                        daoUser.updatePassByUser(username, confirmPassword);
+                        session.setAttribute("successMessage", "Change password successfully!");
+                        response.sendRedirect("index.jsp");
+                    } else {
+                        session.setAttribute("changePassMessage", "The confirmation password is not identical with the new password. Try again!");
+                        response.sendRedirect("ChangePassAn.jsp");
+                    }
+                } else {
+                    session.setAttribute("changePassMessage", "New password should be different from previous one and should not be empty. Try again!");
+                    response.sendRedirect("ChangePassAn.jsp");
+                }
+            } else {
+                session.setAttribute("changePassMessage", "Wrong username or password. Try again!");
+                response.sendRedirect("ChangePassAn.jsp");
+            }
+        }
+        
+        if (service.equals("logout")) {
+            session.invalidate();
+            response.sendRedirect("index.jsp");
+        }
+
+    }
+
+    public boolean validateUser(String username, String password) {
+        boolean flag = false;
+        DAOUser dao = new DAOUser();
+        Vector<User> vec = dao.getAll("select * from [User]");
+
+        for (User user : vec) {
+            if (user.getEmail().equals(username) && user.getPassword().equals(password)) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
     }
 
     @Override
