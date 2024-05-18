73211ba53624af5d88a2aed70ecebad8ae3d559c
Nguyen Quoc Huy (K18 HL) <huynqhe186861@fpt.edu.vn>
2024-05-18 14:06:38 +0700

    fix db script, dbcontext

--- migration.sql
+++ migration.sql
@@ -1,81 +1,152 @@
-use master;
+USE [master]
+GO
 
-drop database if exists QuizPractice;
-create database QuizPractice;
-go
+/****** Object:  Database [Quiz_Practice]    Script Date: 18/05/2024 12:37:26 CH ******/
+DROP DATABASE IF EXISTS [Quiz_Practice]
+GO
 
-use QuizPractice;
-go
+/****** Object:  Database [Quiz_Practice]    Script Date: 18/05/2024 12:37:26 CH ******/
+CREATE DATABASE [Quiz_Practice]
+GO
 
-CREATE TABLE [Users] (
-  [Id] INT PRIMARY KEY IDENTITY(1, 1),
-  [Email] VARCHAR(255) UNIQUE,
-  [Password] VARCHAR(99),
-  [Role] VARCHAR(10) CHECK([Role] in ('Admin', 'Expert', 'Customer', 'Marketing', 'Sale')),
-  [FullName] NVARCHAR(255),
-  [Gender] VARCHAR(255) CHECK ([Gender] IN ('Male', 'Female')),
-  [Mobile] VARCHAR(25),
-  [IsActive] BIT
-  --According to the requirements
-  --The system allows the user to register courses without registering first! See function #12
-  --So my solution is to create a 'temporary user', this type of user will have IsActive set to 0
-  --Login system must not login this type of user, please only allow users with IsActive set to 1 to login
-);
+ALTER DATABASE [Quiz_Practice] SET ANSI_NULL_DEFAULT OFF 
+GO
 
---Profile Pic depends on the user existing, so foreign key constraint is used
-CREATE TABLE [ProfilePic] (
-  [UserId] INT UNIQUE FOREIGN KEY REFERENCES Users(Id) ON DELETE CASCADE,
-  [Image] VARCHAR(8000),
-);
+ALTER DATABASE [Quiz_Practice] SET ANSI_NULLS OFF 
+GO
 
---Reset token depends on the user existing, so foreign key constraint is used
-CREATE TABLE [ResetTokens] (
-	[UserId] INT UNIQUE FOREIGN KEY REFERENCES Users(Id) ON DELETE CASCADE,
-	[Token] VARCHAR(255),
-	[ValidTo] DATETIME,
-);
+ALTER DATABASE [Quiz_Practice] SET ANSI_PADDING OFF 
+GO
 
-CREATE TABLE [Subjects] (
-	[Id] INT PRIMARY KEY IDENTITY(1, 1),
-	[Name] NVARCHAR(255) NOT NULL,
-	[FeaturedSubject] BIT,
-	[Owner] INT FOREIGN KEY REFERENCES Users(Id) ON DELETE SET NULL,
-	[Status] VARCHAR(20) CHECK ([Status] IN ('Published', 'Unpublished')),
-	[Description] NVARCHAR(2048) DEFAULT ''
-);
+ALTER DATABASE [Quiz_Practice] SET ANSI_WARNINGS OFF 
+GO
 
-CREATE TABLE [PricePackages] (
-	[Id] INT PRIMARY KEY,
-	[Name] NVARCHAR(255) NOT NULL,
-	[IsActive] BIT NOT NULL,
-	[ListPrice] INT CHECK([ListPrice] >= 0) NOT NULL,
-	[SalePrice] INT CHECK([SalePrice] >= 0) NOT NULL,
-	[Duration] INT CHECK([Duration] >= 1) NOT NULL,
-	[SubjectId] INT FOREIGN KEY REFERENCES Subjects(Id) ON DELETE CASCADE,
-	--We cannot have two packages with the exact same duration for a single subject
-	--or it will be very weird. Please check this in your application code
-);
+ALTER DATABASE [Quiz_Practice] SET ARITHABORT OFF 
+GO
 
---Registration cannot exist without the person who is registering (a user, temporary or not),
---and a subject that is being registered.
-CREATE TABLE [Registration] (
-	[UserId] INT FOREIGN KEY REFERENCES Users(Id) ON DELETE CASCADE,
-	[PackageId] INT FOREIGN KEY REFERENCES PricePackages(Id) ON DELETE CASCADE,
-	[RegistrationTime] DATE NOT NULL,
-	[Status] VARCHAR(20) CHECK ([Status] IN ('Submitted', 'Cancelled', 'Paid')),
-	PRIMARY KEY([UserId], [PackageId])
-);
+ALTER DATABASE [Quiz_Practice] SET AUTO_CLOSE OFF 
+GO
 
-CREATE TABLE [BlogCategories] (
-	[Id] INT PRIMARY KEY IDENTITY(1, 1),
-	[CategoryName] NVARCHAR(255) UNIQUE NOT NULL
-);
+ALTER DATABASE [Quiz_Practice] SET AUTO_SHRINK OFF 
+GO
 
-CREATE TABLE [Blogs] (
-	[Id] INT PRIMARY KEY IDENTITY(1, 1),
-	[Title] NVARCHAR(512),
-	[AuthorId] INT REFERENCES Users(Id) ON DELETE SET NULL,
-	[UpdatedDate] DATETIME DEFAULT(CURRENT_TIMESTAMP),
-	[CategoryId] INT FOREIGN KEY REFERENCES BlogCategories(Id) ON DELETE SET NULL,
-	[PostText] NTEXT,
-);
+ALTER DATABASE [Quiz_Practice] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [Quiz_Practice] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [Quiz_Practice] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [Quiz_Practice] SET QUERY_STORE = ON
+GO
+
+ALTER DATABASE [Quiz_Practice] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+
+ALTER DATABASE [Quiz_Practice] SET  READ_WRITE 
+GO
+
+USE [Quiz_Practice]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[UserId] [int] NOT NULL primary key,
+	[Email] [varchar](255) NOT NULL,
+	[Password] [varchar](99) NOT NULL,
+	[Role] [varchar](10) NOT NULL,
+	[FullName] [nvarchar](255) NOT NULL,
+	[Gender] [varchar](255),
+	[Mobile] [varchar](25) NOT NULL,
+	[IsActive] [bit] NOT NULL)
+
+GO
+CREATE TABLE [dbo].[ProfilePicture](
+	[PictureId] [int] NOT NULL primary key,
+	[UserId] [int] NOT NULL foreign key references [dbo].[User](UserId),
+	[Image] [varbinary] (MAX))
+
+GO
+CREATE TABLE [dbo].[ResetToken](
+	[TokenId] [int] NOT NULL primary key,
+	[UserId] [int] NOT NULL foreign key references [dbo].[User](UserId),
+	[Token] [varchar] (255),
+	[ValidTo] [datetime])
+
+GO
+CREATE TABLE [dbo].[Registration](
+	[RegistrationId] [int] NOT NULL primary key,
+	[UserId] [int] NOT NULL foreign key references [dbo].[User](UserId),
+	[SubjectId] [varchar] (255) NOT NULL,
+	[RegistrationTime] [date] NOT NULL,
+	[PackageId] [int] NOT NULL,
+	[TotalCost] [float] NOT NULL,
+	[Status] [varchar](25),
+	[ValidFrom] [date],
+	[ValidTo] [date]
+)
