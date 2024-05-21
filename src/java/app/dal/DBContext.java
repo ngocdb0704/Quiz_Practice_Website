56df9bda28c8232b234ebdfab50ef27cf0794352
QuanNMHE182223 <QuanNMHE182223@fpt.edu.vn>
2024-05-21 08:06:42 +0700

    Added ExternalBody.css

--- src/java/app/dal/DBContext.java
+++ src/java/app/dal/DBContext.java
@@ -14,7 +14,7 @@ public class DBContext implements AutoCloseable {
         try {
             //Change the username password and url to connect your own database
             String username = "sa";
-            String password = "123";
+            String password = "123456";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=Quiz_Practice";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
