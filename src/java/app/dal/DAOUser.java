03216d9df06e55db8546ee05cbde7d92b82a44a1
Nguyen Quoc Huy (K18 HL) <huynqhe186861@fpt.edu.vn>
2024-05-27 21:24:20 +0700

    fix a regression in reset password, added basic listing

--- src/java/app/dal/DAOUser.java
+++ src/java/app/dal/DAOUser.java
@@ -13,52 +13,19 @@ import java.util.logging.Logger;
 
 public class DAOUser extends DBContext {
 
-    public List<User> extractResults(ResultSet rs) throws SQLException {
-        List<User> result = new ArrayList<>();
-        while (rs.next()) {
-            User user = new User();
-            user.setUserId(rs.getInt("UserId"));
-            user.setEmail(rs.getString("Email"));
-            user.setPassword(rs.getString("Password"));
-            user.setFullName(rs.getString("FullName"));
-            user.setGender(rs.getString("Gender"));
-            user.setMobile(rs.getString("Mobile"));
-            user.setRole(rs.getString("Role"));
-            user.setIsActive(rs.getBoolean("IsActive"));
-            result.add(user);
+    public boolean updatePasswordById(int id, String password) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement("update [User] set [Password] = ? where [UserId] = ?");
+            stmt.setString(1, password);
+            stmt.setInt(2, id);
+            stmt.executeUpdate();
+
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
-        return result;
-    }
 
-    public User getByEmail(String email) throws SQLException {
-        String sql = "select * from [User] u\n"
-                + "inner join [Gender] g on u.[GenderId] = g.[GenderId]\n"
-                + "inner join [Role] r on u.[RoleId] = r.[RoleId]\n"
-                + "where [Email] = ?";
-
-        PreparedStatement stmt = connection.prepareStatement(sql);
-        stmt.setString(1, email);
-        List<User> result = extractResults(stmt.executeQuery());
-        return result.isEmpty() ? null : result.get(0);
-    }
-
-    public User getById(int id) throws SQLException {
-        String sql = "select * from [User] u\n"
-                + "inner join [Gender] g on u.[GenderId] = g.[GenderId]\n"
-                + "inner join [Role] r on u.[RoleId] = r.[RoleId]\n"
-                + "where [UserId] = ?";
-
-        PreparedStatement stmt = connection.prepareStatement(sql);
-        stmt.setInt(1, id);
-        List<User> result = extractResults(stmt.executeQuery());
-        return result.isEmpty() ? null : result.get(0);
-    }
-
-    public void updatePasswordById(int id, String password) throws SQLException {
-        PreparedStatement stmt = connection.prepareStatement("update [User] set [Password] = ? where [UserId] = ?");
-        stmt.setString(1, password);
-        stmt.setInt(2, id);
-        stmt.executeUpdate();
+        return false;
     }
 
     public void updatePassByUser(String user, String pass) {
