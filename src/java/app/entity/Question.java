4a0e5574ff8061a30c8dee3dc1fc097bed3d3aa6
hoapm <hoapmhe173343@fpt.edu.vn>
2024-06-05 21:18:56 +0700

    layout import question page

---
+++ src/java/app/entity/Question.java
@@ -0,0 +1,112 @@
+
+package app.entity;
+
+/**
+ *
+ * @author Hoapmhe173343
+ */
+public class Question {
+    private int questionID, subjectID;
+    private String questionText;
+    private String optionA, optionB, optionC, optionD;
+    private String correctAnswer, explanation;
+    private int questionLevel;
+
+    public Question() {
+    }
+
+    public Question(int questionID, int subjectID, String questionText, String optionA, String optionB, String optionC, String optionD, String correctAnswer, String explanation, int questionLevel) {
+        this.questionID = questionID;
+        this.subjectID = subjectID;
+        this.questionText = questionText;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        this.correctAnswer = correctAnswer;
+        this.explanation = explanation;
+        this.questionLevel = questionLevel;
+    }
+
+    public int getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
+
+    public int getSubjectID() {
+        return subjectID;
+    }
+
+    public void setSubjectID(int subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public int getQuestionLevel() {
+        return questionLevel;
+    }
+
+    public void setQuestionLevel(int questionLevel) {
+        this.questionLevel = questionLevel;
+    }
+    
+    
+}
