916a0cb8aa14eabc8e4874aca51b6d0fbbecaeca
QuanNMHE182223 <QuanNMHE182223@fpt.edu.vn>
2024-06-10 23:22:04 +0700

    Modified table subject, test display

--- src/java/app/controller/SubjectDetailsController.java
+++ src/java/app/controller/SubjectDetailsController.java
@@ -4,12 +4,15 @@
  */
 package app.controller;
 
+import app.dal.DAOSubject;
+import app.entity.Subject;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
@@ -29,18 +32,19 @@ public class SubjectDetailsController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SubjectDetailsController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SubjectDetailsController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+                
+        HttpSession session = request.getSession();
+        
+        String service = request.getParameter("service");
+        
+        try {
+            DAOSubject daoSubject = new DAOSubject(); 
+            Subject displaySubject = daoSubject.getSubjectById(Integer.parseInt(request.getParameter("subjectId")));
+            session.setAttribute("subjectDetails", displaySubject);
         }
+        catch (Exception e) {}
+        
+        request.getRequestDispatcher("SubjectDetails.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
